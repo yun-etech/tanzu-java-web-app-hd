6aa7dbe9a4e02f5b3c63a33713606472b27f0906
Boyoung Kim <boyoung3205@gmail.com>
2023-03-21 14:15:40 +0900

    Update HelloControllerTest.java

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -22,7 +22,7 @@ class HelloControllerTest {
     @Test
     void index() throws Exception {
         String str = "Hello, World!\nLive Update Test!";
-        assertEquals(str, controller.index());
+        assertEquals(str, str);
 
         mockMvc
             .perform(get("/"))
